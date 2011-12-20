9428c6ba8b903a8854853cf8c045d6cda1da672c
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2011-12-20 11:09:44 -0200

    rails new -O -T -J -S

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ctbenchmark::Application.config.secret_token = '9a5dabec09c2fbcf4c780fe549322f99b212345837c61999b9261199c2330416d715183d2ef3902cbc53434b274e2e63b905f21efe1a56761c81b9025f02274b'
