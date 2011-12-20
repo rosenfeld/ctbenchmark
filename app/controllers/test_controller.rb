d12af7f1fda8e82d278d8a1166eb837176519990
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2011-12-20 11:20:25 -0200

    Actions using catch-throw and not using them

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,8 +1,13 @@
 class TestController < ApplicationController
   def regular
+    render action: 'regular'
   end
 
   def catch_throw
+    catch :rendered do
+      render action: 'catch_throw'
+      throw :rendered
+    end
   end
 
 end
